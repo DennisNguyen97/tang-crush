c520207c07d58081e570972f2f7d0062fb8c3b86
DennisNguyen97 <89007787+DennisNguyen97@users.noreply.github.com>
2021-08-16 14:57:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Em à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnIntro: 'Liu Liu',
+    title: 'Em có đồng ý làm nóc nhà của anh không? 😙',
+    desc: 'Đồng ý hông dạ?',
+    btnYes: 'Vậy là đồng ý đó nha',
+    btnNo: 'Không, không còn lựa chọn nào khác nhĩ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Tại anh là người em thương ấy!',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Đồng ý là nóc nhà của anh nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/dennisTV98' //link mess của các bạn. VD: https://m.me/dennisTV98
 }
