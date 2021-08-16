3dff1bf77950f6106d66dd81bee58f84424517ab
DennisNguyen97 <89007787+DennisNguyen97@users.noreply.github.com>
2021-08-16 15:11:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao em cứ đi nhầm đường.
+    Lạc vào tim anh lẻ loi.
+    Đằng sau chữ yêu đây là thương.`,
     btnIntro: 'Liu Liu',
     title: 'Em có đồng ý làm nóc nhà của anh không? 😙',
     desc: 'Đồng ý hông dạ?',
     btnYes: 'Vậy là đồng ý đó nha',
     btnNo: 'Không, không còn lựa chọn nào khác nhĩ',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại đồng ý làm nóc nhà của anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Tại anh là người em thương ấy!',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
